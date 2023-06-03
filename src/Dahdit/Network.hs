4403254c5ba3cd558204a0e406af47fbd3658c22
Eric Conlon <ejconlon@gmail.com>
2023-06-02 19:51:10 -0700

    etc

--- src/Dahdit/Network.hs
+++ src/Dahdit/Network.hs
@@ -1,35 +1,41 @@
 module Dahdit.Network
-  ( Encoder (..)
+  ( Decoder (..)
+  , runDecoder
+  , handleDecoder
+  , Encoder (..)
   , runEncoder
+  , handleEncoder
   , HostPort (..)
+  , tcpClient
+  , withTcpClient
   , udpClient
   , withUdpClient
   )
 where
 
-import Control.Exception (bracket, onException)
-import Dahdit (Binary (..), Put, putTarget)
+import Control.Monad.IO.Unlift (MonadUnliftIO)
+import Dahdit (Binary (..), Get, Put, putTarget)
+import Data.Acquire (Acquire, mkAcquire, withAcquire)
 import Data.ByteString qualified as BS
 import Network.Socket qualified as NS
 import System.IO (Handle, IOMode (..), hFlush)
 
--- newtype Decoder = Decoder {unDecoder :: forall a. Get a -> IO a}
+newtype Decoder = Decoder {unDecoder :: forall a. Get a -> IO a}
 
--- runDecoder :: Binary a => Decoder -> IO a
--- runDecoder dec = unDecoder dec get
+runDecoder :: Binary a => Decoder -> IO a
+runDecoder dec = unDecoder dec get
+
+handleDecoder :: Handle -> IO Decoder
+handleDecoder = error "TODO"
 
 newtype Encoder = Encoder {unEncoder :: Put -> IO ()}
 
 runEncoder :: Binary a => Encoder -> a -> IO ()
 runEncoder enc = unEncoder enc . put
 
--- -- Need to take something more than handle here. Buffer unused data etc
--- decodeHandle :: Binary a => Handle -> IO a
--- decodeHandle = error "TODO"
-
-encodeHandle :: Handle -> Put -> IO ()
-encodeHandle h p = do
-  let bs = putTarget p
+handleEncoder :: Handle -> Encoder
+handleEncoder h = Encoder $ \p -> do
+  bs <- putTarget p
   BS.hPut h bs
   hFlush h
 
@@ -79,43 +85,41 @@ targetConnect t = do
 socketEncoder :: NS.Socket -> IO Encoder
 socketEncoder sock = do
   handle <- NS.socketToHandle sock WriteMode
-  pure (Encoder (encodeHandle handle))
+  pure (handleEncoder handle)
 
--- socketDecoder :: NS.Socket -> IO Decoder
--- socketDecoder sock = do
---   handle <- NS.socketToHandle sock ReadMode
---   pure (Decoder (decodeHandle handle))
+socketDecoder :: NS.Socket -> IO Decoder
+socketDecoder sock = do
+  handle <- NS.socketToHandle sock ReadMode
+  handleDecoder handle
 
--- socketBidi :: NS.Socket -> IO (Encoder, Decoder)
--- socketBidi sock = do
---   handle <- NS.socketToHandle sock ReadWriteMode
---   pure (Encoder (encodeHandle handle), Decoder (decodeHandle handle))
+socketBidi :: NS.Socket -> IO (Encoder, Decoder)
+socketBidi sock = do
+  handle <- NS.socketToHandle sock ReadWriteMode
+  decoder <- handleDecoder handle
+  pure (handleEncoder handle, decoder)
 
--- -- Should be called with async exceptions masked
--- tcpClient :: HostPort -> TcpOpts -> IO (Encoder, Decoder, IO ())
--- tcpClient hp (TcpOpts finTo) = do
---   sock <- targetConnect (Target hp SockTyTcp)
---   let dis = if finTo > 0 then NS.gracefulClose sock finTo else NS.close sock
---   (enc, dec) <- onException (socketBidi sock) dis
---   pure (enc, dec, dis)
+tcpClient :: HostPort -> TcpOpts -> Acquire (Encoder, Decoder)
+tcpClient hp (TcpOpts finTo) = fmap (\(enc, dec, _) -> (enc, dec)) (mkAcquire acq rel)
+ where
+  acq = do
+    sock <- targetConnect (Target hp SockTyTcp)
+    (enc, dec) <- socketBidi sock
+    pure (enc, dec, sock)
+  rel (_, _, sock) =
+    if finTo > 0 then NS.gracefulClose sock finTo else NS.close sock
 
--- withTcpClient :: HostPort -> TcpOpts -> (Encoder -> Decoder -> IO a) -> IO a
--- withTcpClient hp to f = bracket acq rel use where
---   acq = tcpClient hp to
---   rel (_, _, dis) = dis
---   use (enc, dec, _) = f enc dec
+withTcpClient :: MonadUnliftIO m => HostPort -> TcpOpts -> (Encoder -> Decoder -> m a) -> m a
+withTcpClient hp to f = withAcquire (tcpClient hp to) (uncurry f)
 
 -- Should be called with async exceptions masked
-udpClient :: HostPort -> IO (Encoder, IO ())
-udpClient hp = do
-  sock <- targetConnect (Target hp SockTyUdp)
-  let dis = NS.close sock
-  enc <- onException (socketEncoder sock) dis
-  pure (enc, dis)
+udpClient :: HostPort -> Acquire Encoder
+udpClient hp = fmap fst (mkAcquire acq rel)
+ where
+  acq = do
+    sock <- targetConnect (Target hp SockTyUdp)
+    enc <- socketEncoder sock
+    pure (enc, sock)
+  rel = NS.close . snd
 
 withUdpClient :: HostPort -> (Encoder -> IO a) -> IO a
-withUdpClient hp f = bracket acq rel use
- where
-  acq = udpClient hp
-  rel (_, dis) = dis
-  use (enc, _) = f enc
+withUdpClient = withAcquire . udpClient
